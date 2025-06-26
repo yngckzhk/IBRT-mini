008d6112ac679de1707ec134eff1fba78a04560f
yngckzhk <yngckzhk@yahoo.co.jp>
2025-06-26 14:17:59 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-eki21kms",
+      "name": "eki21kms",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 749.5,
+      "initialViewParameters": {
+        "yaw": 0.9858522500407307,
+        "pitch": 0.021873457344025127,
+        "fov": 1.1967905946803585
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1467315441311854,
+          "pitch": 0.08980086264572229,
+          "rotation": 5.497787143782138,
+          "target": "1-21ks"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-21ks",
+      "name": "和田21ks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 749.5,
+      "initialViewParameters": {
+        "yaw": -0.2733691449166926,
+        "pitch": 0.043746914688044924,
+        "fov": 1.1967905946803585
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8190006423003604,
+          "pitch": 0.1275346062829712,
+          "rotation": 0.7853981633974483,
+          "target": "0-eki21kms"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
